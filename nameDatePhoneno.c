f812d0be18a9a1291bb635b9378e77611677a9b7
bhagyshrilande <145424354+bhagyshrilande@users.noreply.github.com>
2023-09-19 13:06:13 +0530

    Add files via upload

---
+++ nameDatePhoneno.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ void main()
+ {
+     char date[10]="1-06-2003";
+     double phone=9015684926;
+     char name[10]="shree";
+
+     printf(" \n %s",date);
+     printf("\n %.lf",phone);
+     printf(" \n %s",name);
+
+ }
